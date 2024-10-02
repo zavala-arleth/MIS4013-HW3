96d320ed94625c513de4b7a589a1688fa10635e9
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-10-02 13:37:30 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?=$pageTitle?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarToggler">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="post.php">Post</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="post-result.php">Post Result</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Get.php">Get</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Get-Result.php">Get Result</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
