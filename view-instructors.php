dd2a131782898f667a7bd78d29567e8a799ba7f6
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-10-09 14:40:01 -0500

    Update view-instructors.php

--- view-instructors.php
+++ view-instructors.php
@@ -1,1 +1,13 @@
-
+<h>Instructors</h>
+<div class="table-responsive">
+  <table class="table">
+    <thread>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Office</th>
+    </tr>
+    </thread> 
+    <body>
+  </table>
+</div>
