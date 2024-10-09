9e2aeb008e064ef8bcc9456a3c02d0c6fd065244
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-10-09 13:19:08 -0500

    Update Instructors.php

--- Instructors.php
+++ Instructors.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once ("util-db.php");
+require_once("model-instructors.php");
+$pageTitle = "Instructors";
+  include "header.php";
+$instructors= selectInstructors();
+include "view-instructors.php";
+include "footer.php";
+?>
