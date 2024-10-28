45fe6af068d35bceb01e2d1bb93b266435599cdf
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-10-28 13:15:06 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -3,7 +3,7 @@ function get_db_connection(){
     // Create connection
   $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "mis-4013-db1.mysql.database.azure.com", "Arlethadmin", "{your_password}", "{your_database}", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "mis-4013-db1.mysql.database.azure.com", "Arlethadmin", "JAn8qx6T1234", "mis-4013-db1", 3306, MYSQLI_CLIENT_SSL);
     
     // Check connection
     if ($conn->connect_error) {
