3c773168f44a1b38d08374a4add5f3dbd9b40795
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-10-28 13:42:26 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,12 +1,12 @@
 <?php
 function get_db_connection(){
     // Create connection
- $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem ", NULL, NULL);
-mysqli_real_connect($conn, "mis-4013-db1.mysql.database.azure.com", "Arlethadmin", "JAn8qx6T1234", "mis-4013-db1", 3306, MYSQLI_CLIENT_SSL);
-    if ($conn->connect_error) {
+ $con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem ", NULL, NULL);
+mysqli_real_connect($con, "mis-4013-db1.mysql.database.azure.com", "Arlethadmin", "JAn8qx6T1234", "mis-4013-db1", 3306, MYSQLI_CLIENT_SSL);
+    if ($con->connect_error) {
       return false;
     }
-    return $conn;
+    return $con;
 }
 ?>
