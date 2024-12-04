3445c0e22e2f27729a1acb38a505cfab2e39a53b
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-04 09:04:17 -0600

    Create model-reviews.php

---
+++ model-reviews.php
@@ -0,0 +1,15 @@
+<?php
+function selectreviews() {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("SELECT review_id, review_text, rating, review_date FROM 'reviews'");
+    $stmt->execute();
+    $result - $stmt->get_result();
+    $conn->close();
+    return $result;
+  } catch (Exception $e) {
+    $conn->close();
+    throw $e;
+  }
+}
+?>
