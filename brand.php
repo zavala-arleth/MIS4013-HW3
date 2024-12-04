97d37b2d45867d7d50826cd6ca14c40a0382de29
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-04 08:58:19 -0600

    Update brand.php

--- brand.php
+++ brand.php
@@ -3,7 +3,7 @@ require_once ("util-db.php");
 require_once("model-brand.php");
 $pageTitle = "Shoe Brands";
   include "header.php";
-$instructors= selectInstructors();
+$brand= selectbrand();
 include "view-brand.php";
 include "footer.php";
 ?>
