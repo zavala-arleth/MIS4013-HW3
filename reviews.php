eb146a2e03ff97fecc6cf51121fe7a3db238342d
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-04 09:01:18 -0600

    Update reviews.php

--- reviews.php
+++ reviews.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once ("util-db.php");
+require_once("model-reviews.php");
+$pageTitle = "Reviews";
+  include "header.php";
+$reviews= selectreviews();
+include "view-reviews.php";
+include "footer.php";
+?>
