84b4debddeb028e047053db959fb5892ea4f790d
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-04 08:44:10 -0600

    Update and rename Shoe_brand.php to brand.php

--- brand.php
+++ brand.php
@@ -1,9 +1,9 @@
 <?php
 require_once ("util-db.php");
-require_once("model-shoe_brand.php");
+require_once("model-brand.php");
 $pageTitle = "Shoe Brands";
   include "header.php";
 $instructors= selectInstructors();
-include "view-shoe_brand.php";
+include "view-brand.php";
 include "footer.php";
 ?>
