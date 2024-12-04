eff90db16e39a4587dedd09f4977883981f7e976
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-03 18:45:37 -0600

    Update Instructors.php

--- Instructors.php
+++ Instructors.php
@@ -1,9 +1,9 @@
 <?php
 require_once ("util-db.php");
-require_once("model-instructors.php");
-$pageTitle = "Instructors";
+require_once("model-shoes.php");
+$pageTitle = "Shoes";
   include "header.php";
 $instructors= selectInstructors();
-include "view-instructors.php";
+include "view-shoes.php";
 include "footer.php";
 ?>
