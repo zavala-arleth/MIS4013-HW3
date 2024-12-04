2bd2e9882852f9d4805b83b6e2e1a005e57fecf8
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-04 13:12:45 -0600

    Update view-brand.php

--- view-brand.php
+++ view-brand.php
@@ -19,7 +19,7 @@
              <td><? php echo $brand ['brand_name']; <?</td>
              <td><? php echo $brand ['founded_year']; <?</td>
             <td><? php echo $brand ['country']; <?</td>
-            <td><a href="categories-by-brand.php?id=<?php echo $brand[brand_id]; <?">Categories</a></td> 
+            <td><a href="categories-by-brand.php?id=<?php echo $brand['id']; <?">Categories</a></td> 
           </tr>
   </table>
 </div>
