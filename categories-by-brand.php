dc34956b882e80ec345aea4149efb93aee94e54c
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-04 09:55:41 -0600

    Create categories-by-brand.php

---
+++ categories-by-brand.php
@@ -0,0 +1,10 @@
+<?php
+require_none("util.db.php");
+require_once(("model-courses-by-categories.php");
+
+$PageTItle = "Categories by Brands";
+include "view-header.php";
+$categories = selectCategoriesByBrand($_GET['id']);
+include "view-categories-by-brand.php";
+include "view-footer.php";
+?>
