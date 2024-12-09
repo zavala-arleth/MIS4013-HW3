e1aea8f99f1abbbf42a78b7248dd4736373d89dd
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-09 07:34:28 -0600

    Update model-categories.php

--- model-categories.php
+++ model-categories.php
@@ -12,4 +12,20 @@ function selectcategories() {
     throw $e;
   }
 }
+function insertCategories($category_id, $category_name)
+  {
+      try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("INSERT INTO 'categories( 'category_name' ) VALUES (?,?)FROM 'categories'");
+    $stmt->bind_param("ss", $uEmail);
+    $stmt->execute();
+    $result - $stmt->get_result();
+    $conn->close();
+    return $result;
+  } catch (Exception $e) {
+    $conn->close();
+    throw $e;
+  }
+}
+  
 ?>
