98ee4385dfca8cf8ad3a0df7ed83d2a7be552ea8
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-12 10:37:35 -0600

    Update view-categories.php

--- view-categories.php
+++ view-categories.php
@@ -1,13 +1,13 @@
 <h>Categories</h>
 <div class="table-responsive">
   <table class="table">
-    <thread>
+    <thead>
     <tr>
       <th>Category ID</th>
       <th>Name</th>
     </tr>
-    </thread> 
-    <body>
+    </thead> 
+    <tbody>
       <?php
         while ($categories = $categories->fetch_assoc()) {
           ?>
@@ -15,5 +15,6 @@
             <td><? php echo $categories ['category_id']; <?</td>
              <td><? php echo $categories ['category_name']; <?</td>
           </tr>
+            </tbody>
   </table>
 </div>
