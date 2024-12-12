b4fca41e035a690eeebf841aac86899478398d48
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-12 14:51:22 -0600

    Update view-categories.php

--- view-categories.php
+++ view-categories.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once 'model-categories.php';
 ?>
@@ -6,23 +5,23 @@ require_once 'model-categories.php';
 <div class="table-responsive">
   <table class="table">
     <thead>
-    <tr>
-      <th>Category ID</th>
-      <th>Category Name</th>
-    </tr>
-    </thead> 
+      <tr>
+        <th>Category ID</th>
+        <th>Category Name</th>
+      </tr>
+    </thead>
     <tbody>
       <?php
-        $result=  selectbrand();
-        while ($brand = $result->fetch_assoc()) {
-          ?>
-          <tr>
-             <td><? php echo $categories ['category_id']; ?></td>
-             <td><? php echo $categories ['category_name']; ?></td>
-          </tr>
-          <?php
-          }
-          ?>
-    </tbody> 
+      $result = selectCategories();
+      while ($category = $result->fetch_assoc()) {
+        ?>
+        <tr>
+          <td><?php echo $category['category_id']; ?></td>
+          <td><?php echo $category['category_name']; ?></td>
+        </tr>
+        <?php
+      }
+      ?>
+    </tbody>
   </table>
 </div>
