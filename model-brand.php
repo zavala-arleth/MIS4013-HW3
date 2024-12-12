9c2ade7e734178e40040dad3e2bcbf02cd6f0353
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-11 21:33:53 -0600

    Update model-brand.php

--- model-brand.php
+++ model-brand.php
@@ -7,7 +7,7 @@ function selectbrand() {
     }
 
     try {
-        $conn = get_db_connection();
+       
         $stmt = $conn->prepare("SELECT id, brand_name, founded_year, country FROM brand");
         $stmt->execute();
         $result = $stmt->get_result();
