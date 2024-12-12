8739bdeefb46055b455ca9e63c7757fc350a5e1e
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-12 01:36:28 -0600

    Update view-brand.php

--- view-brand.php
+++ view-brand.php
@@ -22,7 +22,6 @@ require_once 'model-brand.php';
              <td><?php echo $brand ['brand_name']; ?></td>
              <td><?php echo $brand ['founded_year']; ?></td>
             <td><?php echo $brand ['country']; ?></td>
-            //<td><a href="categories-by-brand.php?id=<?php echo $brand['id']; <?">Categories</a></td> 
           </tr>
           <?php
           }
