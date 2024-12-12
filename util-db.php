ab6ba21351de6d707c93353f64c06f37e91a7944
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-12 01:30:07 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,6 +1,6 @@
 <?php
 function get_db_connection() {
-    $conn = new mysqli('mis-4013-db1.mysql.database.azure.com', 'Arlethadmin', 'JAn8qx6T1234%', 'Homework3');
+    $conn = new mysqli('mis-4013-db1.mysql.database.azure.com', 'Arlethadmin', 'JAn8qx6T1234%', 'shoes');
     if ($conn->connect_error) {
         return false;
     }
