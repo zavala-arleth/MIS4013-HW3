06462969a5218f4635b16a886da2f2171a5a19cc
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 15:54:30 -0600

    Update and rename view-categories-by-brand.php to view-product-by-brand.php

---
+++ view-product-by-brand.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'model-product-by-brand.php';
+?>
+<h1>Products by Brands </h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+    <tr>
+      <th>Product ID</th>
+      <th>Product Name</th>
+      <th>Category ID</th>
+      <th>Brand ID</th>
+    </tr>
+    </thead> 
+    <tbody>
+      <?php
+        $result=  selectproduct();
+        while ($product = $result->fetch_assoc()) {
+          ?>
+          <tr>
+            <td><?php echo $product ['product_id']; ?></td>
+             <td><?php echo $product ['product_name']; ?></td>
+             <td><?php echo $product ['category_id']; ?></td>
+            <td><?php echo $product ['brand_id']; ?></td>
+          </tr>
+          <?php
+          }
+          ?>
+    </tbody> 
+  </table>
+</div>
