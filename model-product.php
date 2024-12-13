36ed7aecf5bda00fd876e51e312a367431f401b6
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 11:22:27 -0600

    Update model-product.php

--- model-product.php
+++ model-product.php
@@ -1,1 +1,21 @@
+<?php
+require_once 'util-db.php';
+function selectproduct() {
+       $conn = get_db_connection();
+    if (!$conn) {
+        throw new Exception("Database connection failed.");
+    }
 
+    try {
+       
+        $stmt = $conn->prepare("SELECT product_id, product_name, category_id, brand_id FROM product");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
