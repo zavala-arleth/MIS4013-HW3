539e89933467af51180a25b97ad956d5c92738b1
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 16:19:12 -0600

    Update view-product-by-brand.php

--- view-product-by-brand.php
+++ view-product-by-brand.php
@@ -8,7 +8,6 @@ require_once 'model-product-by-brand.php';
     <tr>
       <th>Product ID</th>
       <th>Product Name</th>
-      <th>Category ID</th>
       <th>Brand ID</th>
     </tr>
     </thead> 
@@ -20,7 +19,6 @@ require_once 'model-product-by-brand.php';
           <tr>
             <td><?php echo $product ['product_id']; ?></td>
              <td><?php echo $product ['product_name']; ?></td>
-             <td><?php echo $product ['category_id']; ?></td>
             <td><?php echo $product ['brand_id']; ?></td>
           </tr>
           <?php
