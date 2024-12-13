caf04cd5e11f6d0efba030d52fdba51eaf1600d0
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-12 23:07:54 -0600

    Update model-reviews.php

--- model-reviews.php
+++ model-reviews.php
@@ -1,10 +1,11 @@
 <?php
+require_once 'util-db.php';
 function selectreviews() {
   try {
     $conn = get_db_connection();
     $stmt = $conn->prepare("SELECT review_id, review_text, rating, review_date FROM reviews");
     $stmt->execute();
-    $result - $stmt->get_result();
+    $result = $stmt->get_result();
     $conn->close();
     return $result;
   } catch (Exception $e) {
