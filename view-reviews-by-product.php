2370c5e656874711c85f62fa8d576b9e1b7ffbc3
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 17:42:54 -0600

    Update view-reviews-by-product.php

--- view-reviews-by-product.php
+++ view-reviews-by-product.php
@@ -1,1 +1,33 @@
-
+<?php
+require_once 'model-reviews-by-product.php';
+?>
+<h1>Reviews</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Review ID</th>
+        <th>Rating</th>
+        <th>Review Text</th>
+        <th>Review Date</th>
+        <th>Product ID</th>
+      </tr>
+    </thead> 
+    <tbody>
+      <?php
+      $result = selectreviews();
+      while ($review = $result->fetch_assoc()) { 
+        ?>
+        <tr>
+          <td><?php echo $review['review_id']; ?></td>
+          <td><?php echo $review['rating']; ?></td>
+          <td><?php echo $review['review_text']; ?></td>
+          <td><?php echo $review['review_date']; ?></td>
+          <td><?php echo $review['product_id']; ?></td>
+        </tr>
+        <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
