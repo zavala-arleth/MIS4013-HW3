740b6e44fc4c92a0b38822c4dedfb7e69f3ec116
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 15:48:34 -0600

    Update model-categories-by-brand.php

--- model-categories-by-brand.php
+++ model-categories-by-brand.php
@@ -3,7 +3,7 @@
 function selectcategoriesByBrand($iid) {
   try {
     $conn = get_db_connection();
-    $stmt = $conn->prepare("SELECT c.category_id, c.category_name FROM categories c join reviews r ON c.category_id = r.category_id where r.review_id=?
+    $stmt = $conn->prepare("SELECT p.product_id, p.product_name, b.id AS brand_id, b.brand_name FROM product p JOIN brands b ON p.brand_id = b.id WHERE p.product_id = ?
 ");
     $stmt->bind_param("i", $iid);
     $stmt->execute();
