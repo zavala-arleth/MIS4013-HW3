7e3b93f3976f3ccb1f3e9bac589642c62475cf20
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 15:55:31 -0600

    Update and rename model-categories-by-brand.php to model-product-by-brand.php

--- model-product-by-brand.php
+++ model-product-by-brand.php
@@ -1,6 +1,6 @@
 
 <?php
-function selectcategoriesByBrand($iid) {
+function selectproductByBrand($iid) {
   try {
     $conn = get_db_connection();
     $stmt = $conn->prepare("SELECT p.product_id, p.product_name, b.id AS brand_id, b.brand_name FROM product p JOIN brands b ON p.brand_id = b.id WHERE p.product_id = ?
