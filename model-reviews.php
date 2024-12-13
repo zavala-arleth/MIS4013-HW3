6c0fe98f18197e91ad761a7f729fe909fca07568
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-12 23:57:27 -0600

    Update model-reviews.php

--- model-reviews.php
+++ model-reviews.php
@@ -3,7 +3,9 @@ require_once 'util-db.php';
 function selectreviews() {
   try {
     $conn = get_db_connection();
-    $stmt = $conn->prepare("SELECT review_id, review_text, rating, review_date FROM reviews");
+    $stmt = $conn->prepare("    SELECT r.review_id, r.rating, r.review_text, r.review_date, c.category_name
+    FROM reviews r
+    INNER JOIN categories c ON r.category_id = c.category_id");
     $stmt->execute();
     $result = $stmt->get_result();
     $conn->close();
