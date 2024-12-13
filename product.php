68e5646970a2e86bd09f0c25cb6ca660fce6109e
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 11:20:00 -0600

    Update product.php

--- product.php
+++ product.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once ("util-db.php");
+require_once("model-product.php");
+$pageTitle = "Products";
+  include "header.php";
+$brand= selectproduct();
+include "view-product.php";
+include "footer.php";
+?>
