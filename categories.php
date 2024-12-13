e9c1afd7f2c6fd8abca706d61682c6de6a2c740d
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-12 22:51:57 -0600

    Update categories.php

--- categories.php
+++ categories.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-categories.php");
 
 $PageTitle = "Categories";
-include "view-header.php";
+include "header.php";
 $categories = selectcategories();
 include "view-categories.php";
 include "view-footer.php";
