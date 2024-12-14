684907956587c33f6d5abb13d9896d8314814e96
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 18:06:06 -0600

    Update view-brand.php

--- view-brand.php
+++ view-brand.php
@@ -23,7 +23,8 @@ require_once 'model-brand.php';
              <td><?php echo $brand ['founded_year']; ?></td>
             <td><?php echo $brand ['country']; ?></td>
           </td>
-      <form>
+      <form method="post" action="sections-by-course.php">
+        <input type="hidden" name="bid" value="<?php echo $brand ['id']; ?>">
         <button type="submit" class="btn-primary">Sections</button>
       </form>
     </tbody>
