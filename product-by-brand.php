78790cb951cf7dcccfb0ae75ac9ced60db77ac98
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 21:14:27 -0600

    Update product-by-brand.php

--- product-by-brand.php
+++ product-by-brand.php
@@ -3,8 +3,8 @@ require_once("util-db.php");
 require_once("model-product-by-brand.php");
 
 $PageTItle = "Product by Brands";
-include "view-header.php";
+include "header.php";
 $categories = selectproductByBrand($_GET['bid']);
 include "view-product-by-brand.php";
-include "view-footer.php";
+include "footer.php";
 ?>
