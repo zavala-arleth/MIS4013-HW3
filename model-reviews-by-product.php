6954ac7215936657fa8d9518d370e75a47882273
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 23:44:31 -0600

    Update model-reviews-by-product.php

--- model-reviews-by-product.php
+++ model-reviews-by-product.php
@@ -2,7 +2,7 @@
 function selectreviewsByProduct($pid) {
   try {
     $conn = get_db_connection();
-    $stmt = $conn->prepare("SELECT p.product_id, p.product_name, p.category_id, b.id AS brand_id, b.brand_name FROM product p JOIN brands b ON p.brand_id = b.id WHERE b.id = ?
+    $stmt = $conn->prepare("SELECT r.review_id, r.rating, r.review_text, r.review_date, r.product_id FROM reviews r  WHERE r.product_id = ?"
 ");
     $stmt->bind_param("i", $pid);
     $stmt->execute();
