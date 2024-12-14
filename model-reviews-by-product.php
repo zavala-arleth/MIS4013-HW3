b20085b4c91f21d33f523567e386405bd319f248
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 18:34:02 -0600

    Update model-reviews-by-product.php

--- model-reviews-by-product.php
+++ model-reviews-by-product.php
@@ -1,10 +1,19 @@
-<?php
-require_once("util-db.php");
-require_once("model-reviews-by-product.php");
 
-$PageTitle = "Reviews by Product";
-include "view-header.php";
-$reviews = selectreviewsByProduct($_POST['id']);
-include "view-reviews-by-product.php";
-include "view-footer.php";
+
+<?php
+function selectproductByBrand($pid) {
+  try {
+    $conn = get_db_connection();
+    $stmt = $conn->prepare("SELECT p.product_id, p.product_name, p.category_id, b.id AS brand_id, b.brand_name FROM product p JOIN brands b ON p.brand_id = b.id WHERE b.id = ?
+");
+    $stmt->bind_param("i", $pid);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $conn->close();
+    return $result;
+  } catch (Exception $e) {
+    $conn->close();
+    throw $e;
+  }
+}
 ?>
