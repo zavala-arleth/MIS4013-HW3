0584a93077ec577dda8f56416cb3364e87aa3717
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 18:32:06 -0600

    Update model-reviews-by-product.php

--- model-reviews-by-product.php
+++ model-reviews-by-product.php
@@ -2,9 +2,9 @@
 require_once("util-db.php");
 require_once("model-reviews-by-product.php");
 
-$PageTItle = "Reviews by Product";
+$PageTitle = "Reviews by Product";
 include "view-header.php";
-$categories = selectreviewsByProduct($_POST['id']);
+$reviews = selectreviewsByProduct($_POST['id']);
 include "view-reviews-by-product.php";
 include "view-footer.php";
 ?>
