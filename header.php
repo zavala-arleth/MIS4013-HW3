dcf708cf009b50eb7658dccc5f74e121c897af6d
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-12-13 21:58:22 -0600

    Update header.php

--- header.php
+++ header.php
@@ -5,12 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+.navbar {
+    background-color: #f8f9fa; 
+}
+.navbar .nav-link {
+    color: grey;
+}
+.navbar .nav-link:hover {
+    color: black; 
+}
   </head>
   <body>
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#">Final Project</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
